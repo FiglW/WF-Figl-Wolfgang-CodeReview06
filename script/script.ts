96592b740a48bf8798739000029811ed78066fa8
Wolfgang Figl <wolfgang.figl@me.com>
2019-11-05 09:39:33 +0100

    joooo

--- script/script.ts
+++ script/script.ts
@@ -65,8 +65,8 @@ class restaurant extends place{
 
 }
 let moon = new restaurant("Netherlands","Amsterdam","Overhoeksplein 3, 1031 KS","./img/moonrest.jpg","Moon","Gourmet","12–2pm, 6–9pm","phone: +31 20 237 6311","https://restaurantmoon.nl/")
-let zaza = new restaurant("Netherlands","Amsterdam","Daniël Stalpertstraat 103,1072 XD",".img/zaza.png","Zaza","casual","6:15 pm – 10:00 pm","phone: +31 (0) 20 673 6333","http://zazas.nl/")
-let bakers = new restaurant("Netherlands","Amsterdam","Eerste Jacob van Campenstraat 54,1072 BH",".img/bakers.jpg","Bakers and Roasters","breakfast","8:30 am - 4pm","phone: +31 20 772 2627","http://www.bakersandroasters.com/")
+let zaza = new restaurant("Netherlands","Amsterdam","Daniël Stalpertstraat 103,1072 XD","./img/zaza.png","Zaza","casual","6:15 pm – 10:00 pm","phone: +31 (0) 20 673 6333","http://zazas.nl/")
+let bakers = new restaurant("Netherlands","Amsterdam","Eerste Jacob van Campenstraat 54,1072 BH","./img/bakers.jpg","Bakers and Roasters","breakfast","8:30 am - 4pm","phone: +31 20 772 2627","http://www.bakersandroasters.com/")
 
 data.push(moon)
 data.push(zaza)
@@ -92,9 +92,9 @@ class club extends place{
 	}
 }
 
-let melkweg = new club("Netherlands","Amsterdam","Lijnbaansgracht 234A, 1017 PH",".img/melkweg.jpg","Melkweg","from hippies to punk, from grunge to hip hop.","10-60€","no dresscode")
-let wester = new club("Netherlands","Amsterdam","Klönneplein 4-6, 1014 DD",".img/wester.jpg","Wester Unie","House & Techno","10-30€","no dresscode")
-let jimmy = new club("Netherlands","Amsterdam","Korte Leidsedwarsstraat 18, 1017 RC",".img/jimmy.jpg","Jimmy Woo","R&B,Hip-Hop,Techno","10-20€","casual")
+let melkweg = new club("Netherlands","Amsterdam","Lijnbaansgracht 234A, 1017 PH","./img/melkweg.jpg","Melkweg","from hippies to punk, from grunge to hip hop.","10-60€","no dresscode")
+let wester = new club("Netherlands","Amsterdam","Klönneplein 4-6, 1014 DD","./img/wester.jpg","Wester Unie","House & Techno","10-30€","no dresscode")
+let jimmy = new club("Netherlands","Amsterdam","Korte Leidsedwarsstraat 18, 1017 RC","./img/jimmy.jpg","Jimmy Woo","R&B,Hip-Hop,Techno","10-20€","casual")
 
 data.push(melkweg)
 data.push(wester)
@@ -120,9 +120,9 @@ class events extends place{
 	}
 }
 
-let lightshow = new events("Netherlands","Amsterdam","Amsterdam",".img/amsterdamevent.jpg","Amsterdam Light Festival","winter outdoor festival of modern art, light and water."," Nov 28, 2019-Jan 19, 2020","for free",)
-let christmas = new events("Netherlands","Amsterdam","Throughout Amsterdam",".img/christmas.jpg","Amsterdam Christmas Market","Christmas","Late November until early January","free entry")
-let kings = new events("Netherlands","Amsterdam","Amsterdam",".img/kings.jpg","Kings Day","Kings Day is a National Holiday","27th April","for free")
+let lightshow = new events("Netherlands","Amsterdam","Amsterdam","./img/amsterdamevent.jpg","Amsterdam Light Festival","winter outdoor festival of modern art, light and water."," Nov 28, 2019-Jan 19, 2020","for free",)
+let christmas = new events("Netherlands","Amsterdam","Throughout Amsterdam","./img/christmas.jpg","Amsterdam Christmas Market","Christmas","Late November until early January","free entry")
+let kings = new events("Netherlands","Amsterdam","Amsterdam","./img/kings.jpg","Kings Day","Kings Day is a National Holiday","27th April","for free")
 
 data.push(lightshow)
 data.push(christmas)
